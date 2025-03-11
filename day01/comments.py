894d7cf54189dbfee16cdef7a0f73039d7f9c94f
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-03-11 09:11:43 -0400

    .

---
+++ day01/comments.py
@@ -0,0 +1,16 @@
+# print("Hello, World!")
+print("Cheers, Mate!")  # Single line Comments
+
+# Multiline Comments
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+# This is a comment
+# written in
+# more than just one line
+print("Hello, World!")
---
+++ day01/hello.py
@@ -0,0 +1,10 @@
+print("Hello World")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print("This is test")
+
+if 5 > 2:
+    print("Five is greater than two!")
+    if 4 > 2:
+        print("Five is greater than two!")
