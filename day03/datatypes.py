304d463c4958b72b402f94dff603b15bc0c8183c
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-03-12 11:50:37 -0400

    .

---
+++ day03/datatypes.py
@@ -0,0 +1,23 @@
+# Python Data Types
+# Built-in Data Types
+"""
+x = 5
+print(type(x))
+"""
+# SET
+"""
+x = set(("apple", "banana", "cherry"))
+print(x)
+x.add("oranges")
+print(x)
+x.remove("apple")
+print(x)
+"""
+
+# FROZENSET
+x = frozenset(("apple", "banana", "cherry"))
+print(x)
+# x.add("oranges") #It wont allow you to add
+# print(x)
+# x.remove("apple") #It wont allow you to remove
+# print(x)
