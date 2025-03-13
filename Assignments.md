3456015ec9363b7c2d39a9a8eea50d0c64b4f574
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-03-13 11:44:56 -0400

    .

--- Assignments.md
+++ Assignments.md
@@ -4,3 +4,11 @@ Find out the types of modules and list some common modules any new python develo
 
 ### Problems to Solve
 P1. Using the OS module pickup the dir and sub-dir from a given path.
+P2. Add two number and print the output(result)
+P3. Declare two number variables and find the remainder.
+P4. Use the Input to add two numbers.
+P5. Use the Input to find the average.
+P6. Create a square root or cube of a number.
+
+Sort files based on their extension type. [to begin with you can use txt, doc, ppt, jpg...] 
+Sort files based on their extension type. [to begin with you can use txt, doc, ppt, jpg...] and move them into appropriate folders [month and year] based on their creation date.
---
+++ day04/string.py
@@ -0,0 +1,33 @@
+# students = "Arash,Sam,Aimee,Kennedy"
+students = "Arash |sam |aimee|Kennedy"
+student = students.split("|")
+
+for x in student:
+    print(x.strip().capitalize())
+
+"""
+for x in "banana":
+    print(x)
+"""
+
+# poem = """
+"""
+Yesterday, upon the stair,
+I met a man who wasn't there
+He wasn't there again today
+I wish, I wish he'd go away...
+
+When I came home last night at three
+The man was waiting there for me
+But when I looked around the hall
+I couldn't see him there at all!
+Go away, go away, don't you come back any more!
+Go away, go away, and please don't slam the door... (slam!)
+
+Last night I saw upon the stair
+A little man who wasn't there
+He wasn't there again today
+Oh, how I wish he'd go away...
+"""
+
+# print(poem)
