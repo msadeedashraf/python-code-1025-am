1066ce9db6e0dfc1b204d4eba444a32082377e2f
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-03-14 10:24:54 -0400

    .

---
+++ day05/collections.py
@@ -0,0 +1,51 @@
+# Collections
+"""
+Python Collections (Arrays)
+There are four collection data types in the Python programming language:
+
+List is a collection which is ordered and changeable. Allows duplicate members.
+Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+Dictionary is a collection which is ordered** and changeable. No duplicate members.
+"""
+# Python Lists
+# https://www.w3schools.com/python/python_lists.asp
+# Python Tuples
+# https://www.w3schools.com/python/python_tuples.asp
+# Python Sets
+# https://www.w3schools.com/python/python_sets.asp
+# Python Dictionaries
+# https://www.w3schools.com/python/python_dictionaries.asp
+
+
+thislist = ["apple", "banana", "cherry"]
+# mylist = list(thislist)
+mylist = thislist
+print(mylist)
+
+"""
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+    if "c" in x:
+        newlist.append(x)
+
+print(newlist)
+"""
+
+"""
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+
+print(thislist.)
+print(thislist[0])
+print(len(thislist))
+
+
+mySecondList = list(("apple", "banana", "cherry", "apple", "cherry"))
+print(mySecondList) 
+
+
+"""
---
+++ day05/string.py
@@ -0,0 +1,5 @@
+myName = input("Enter your name:")
+myMsg = f"Good Morning {myName}"
+
+print(myMsg)
+print(myMsg.find("Sadeed") + 1)
